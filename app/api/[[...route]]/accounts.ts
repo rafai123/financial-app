8e61f9fa814c2576e6683812cdca6c2ddf2151c4
Muhammad Rafai <rafaimhd123@gmail.com>
2024-09-27 22:18:12 +0700

    add route to accounts/:id

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -34,6 +34,44 @@ const app = new Hono()
 
         return c.json({data})
     })
+    .get(
+        "/:id",
+        clerkMiddleware(),
+        zValidator("param", z.object({
+            id: z.string().optional()
+        })),
+        async c => {
+            const auth = getAuth(c)
+            const { id } = c.req.valid("param")
+
+            if (!id) {
+                return c.json({error: "Missing ID"}, 400)
+            }
+
+            if (!auth?.userId) {
+                return c.json({error: "Unauthorized"}, 401)
+            }
+
+            const [data] = await db
+                .select({
+                    id: schema.accounts.id,
+                    name: schema.accounts.name,
+                })
+                .from(schema.accounts)
+                .where(
+                    and(
+                        eq(schema.accounts.userId, auth.userId),
+                        eq(schema.accounts.id, id)
+                    ),
+                );
+
+            if (!data) {
+                return c.json({error: "Data Not Found"}, 404)
+            }
+
+            return c.json({ data })
+        }
+    )
     .post("/",
     clerkMiddleware(),
     zValidator("json", schema.insertAccountSchema.pick({
