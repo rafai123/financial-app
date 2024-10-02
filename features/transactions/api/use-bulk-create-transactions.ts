dde740415e982347eb6ac45be4079e40cc4f9738
Muhammad Rafai <rafaimhd123@gmail.com>
2024-10-02 22:39:29 +0700

    add bulk create

---
+++ features/transactions/api/use-bulk-create-transactions.ts
@@ -0,0 +1,28 @@
+import { client } from "@/lib/hono";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { InferRequestType, InferResponseType } from "hono";
+import { toast } from "sonner";
+
+type ResponseType = InferResponseType<typeof client.api.transactions["bulk-create"]["$post"]>
+type RequestType = InferRequestType<typeof client.api.transactions["bulk-create"]["$post"]>["json"]
+
+export const useBulkCreateTransactions = () => {
+    const queryClient = useQueryClient()
+
+    const mutation = useMutation<ResponseType, Error, RequestType>({
+        mutationFn: async (json) => {
+            const response = await client.api.transactions["bulk-create"]["$post"]({ json })
+            return await response.json()
+        },
+        onSuccess: () => {
+            queryClient.invalidateQueries({queryKey: ["transactions"]})
+            toast.success("Transactions Inserted")
+            // TODO : also invalidate summary
+        },
+        onError: () => {
+            toast.error("Failed to insert Transactions")
+        }
+    })
+
+    return mutation
+}
